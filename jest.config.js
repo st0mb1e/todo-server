fb58c7f8b6946788bcb189236a7e054f4e8dd48e
Владислав Ярцев <stombie@yandex.ru>
2025-11-27 00:25:52 +0300

    fix build

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import('jest').Config} **/
 module.exports = {
     testEnvironment: 'node',
+    testMatch: [
+        '<rootDir>/tests/**/*.test.ts'
+    ],
+    moduleFileExtensions: ['ts', 'js', 'json'],
     transform: {
         ...tsJestTransformCfg,
     },
